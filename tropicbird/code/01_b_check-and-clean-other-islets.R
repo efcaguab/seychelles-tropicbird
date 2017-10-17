d65826c465535b4e23455a704aec7f608716eb23
Fernando Cagua <efcaguab@gmail.com>
2017-10-18 12:59:47 +1300

    Clean data from Cousin and Denis

--- tropicbird/code/01_b_check-and-clean-other-islets.R
+++ tropicbird/code/01_b_check-and-clean-other-islets.R
@@ -1,5 +1,9 @@
 aride <- read.csv('../data/raw/aride.csv')
 cousine <- read.csv('../data/raw/cousine.csv')
+cousin <- read.csv('../data/raw/cousin.csv')
+denis <- read.csv('../data/raw/denis.csv')
+
+library(magrittr)
 
 cousine %<>%
 	dplyr::rename(date = mydate) %>%
@@ -27,3 +31,28 @@ saveRDS(cousine, file = '../data/processed/cousine.rds',
 				compress = F, ascii = T)
 saveRDS(aride, file = '../data/processed/aride.rds',
 				compress = F, ascii = T)
+
+cousin %<>% 
+	dplyr::mutate(date = paste(year, '06', '15', sep = "-"), 
+								date = as.POSIXct(date, tz = "Indian/Mahe")) %>%
+	dplyr::filter(area == 'NW') %>%
+	dplyr::select(date, p_fledged) %>%
+	dplyr::mutate(location = "Cousin",
+								spp_name = "White-tail tropicbird")
+
+saveRDS(cousin, file = '../data/processed/cousin-success.rds', 
+				compress = F, ascii = T)
+
+denis %<>%
+	dplyr::mutate(date = paste(Year, Month, '15'), 
+								date = as.POSIXct(date, format = "%Y %b %d", tz = "Indian/Mahe"),
+								n_new_nest = Fledged + Failed) %>%
+	dplyr::rename(n_fledged = Fledged) %>%
+	dplyr::select(date, n_new_nest, n_fledged) %>%
+	dplyr::mutate(p_fledged = n_fledged/n_new_nest) %>%
+	dplyr::summarise_all(mean) %>%
+	dplyr::mutate(location = "Denis",
+								spp_name = "White-tail tropicbird")
+
+saveRDS(denis, file = '../data/processed/denis.rds')
+	
