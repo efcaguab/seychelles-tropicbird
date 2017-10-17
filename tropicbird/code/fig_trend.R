1b7c0ac05a3de0d3f3d8cca0f9399b5943bbbd81
Fernando Cagua <efcaguab@gmail.com>
2017-10-18 12:59:13 +1300

    Add Cousin and Denis to the graphs

--- tropicbird/code/fig_trend.R
+++ tropicbird/code/fig_trend.R
@@ -45,23 +45,24 @@ mean_fits %<>%
 islet_fits %<>%
 	mu(var = as.POSIXct(var, origin = "1970-01-01", tz = "Indian/Mahe") )
 
+denis <- readRDS(file = '../data/processed/denis.rds') %>%
+	dplyr::mutate(location2 = location)
+
 pm <- ggplot(mean_fits, aes(var, exp(fit), colour = spp_name, fill = spp_name, linetype = location)) +
 	# geom_line(aes(group = islet), size = 0.25, alpha = 1, show.legend = F) +
 	geom_line(data = mean_fits, aes(y = exp(fit)), 
 						size = 0.5, alpha = 1, show.legend = T) +
 	geom_ribbon(data = mean_fits, aes(ymin = exp(fit-se), ymax = exp(fit+se)),
 							colour = 'transparent', alpha = 0.2) +
+	geom_point(data = denis, aes(x = date, y = n_new_nest), shape = 2) +
 	# facet_wrap(~spp_name, scales = 'free', ncol = 2) +
 	scale_x_datetime(name = '', expand = c(0,0)) +
 	scale_y_continuous(name = 'new nests (month)') +
 	scale_colour_brewer(palette = 'Set1', name = "", guide = guide_legend(title = NULL, direction = 'horizontal')) +
 	scale_fill_brewer(palette = 'Set1', name = "", guide = guide_legend(title = NULL, direction = 'horizontal')) +
-	scale_linetype(name = "", guide = guide_legend(title = NULL, direction = 'horizontal')) +
-	# guides(fill = guide_legend(direction = 'horizontal', ncol = 1)) +
+	scale_linetype_manual(name = "", guide = guide_legend(title = NULL, direction = 'horizontal'), values = c(1,2,3,4)) +
 	theme_fe +
-	theme(legend.position = 'top', legend.direction = 'vertical', legend.box = "vertical", 
-				legend.margin = margin(), 
-				legend.box.margin = margin())
+	theme(legend.position = 'none')
 
 pm
 
@@ -160,30 +161,42 @@ mean_fits %<>%
 islet_fits %<>%
 	mu(var = as.POSIXct(var, origin = "1970-01-01", tz = "Indian/Mahe") )
 
-pmsf <- ggplot(mean_fits, aes(var, colour = spp_name, fill = spp_name, linetype = location)) +
+cousin_succ <- readRDS(file = '../data/processed/cousin-success.rds') %>%
+	dplyr::filter(date > as.POSIXct("2000-01-01")) %>%
+	dplyr::mutate(location2 = location) 
+
+pmsf <- ggplot(mean_fits, aes(var, colour = spp_name, fill = spp_name)) +
 	# geom_line(aes(group = islet), size = 0.25, alpha = 1, show.legend = F) +
-	geom_line(data = mean_fits, aes(y = plogis(fit)), 
-						size = 0.5, alpha = 1, show.legend = F) +
-	geom_ribbon(data = mean_fits, aes(ymin = plogis(fit-se), ymax = plogis(fit+se)),
+	geom_line(data = mean_fits, aes(y = plogis(fit), linetype = location), 
+						size = 0.5, alpha = 1, show.legend = T) +
+	geom_ribbon(data = mean_fits, aes(ymin = plogis(fit-se), ymax = plogis(fit+se), linetype = location),
 							colour = 'transparent', alpha = 0.2) +
 	# facet_wrap(~spp_name, scales = 'free', ncol = 2) +
-	scale_x_datetime(name = '', expand = c(0,0)) +
-	scale_y_continuous(name = 'fledging success', labels = scales::percent) +
-	scale_colour_brewer(palette = 'Set1') +
-	scale_fill_brewer(palette = 'Set1') +
+	# geom_line(data = cousin_succ, aes(x = date, y = p_fledged/100, linetype = location)) +
+	geom_point(data = cousin_succ, aes(x = date, y = p_fledged/100, shape = location2)) +
+	geom_point(data = denis, aes(x = date, y = p_fledged, shape = location2)) +
+	scale_colour_brewer(palette = 'Set1', name = "", guide = guide_legend(title = NULL, direction = 'horizontal')) +
+	scale_fill_brewer(palette = 'Set1', name = "", guide = guide_legend(title = NULL, direction = 'horizontal')) +
+	scale_shape_manual(values = c(1,2), name = "", guide = guide_legend(title = NULL, direction = 'horizontal')) +
+	scale_linetype_manual(name = "", guide = guide_legend(title = NULL, direction = 'horizontal'), values = c(1,2,3,4)) +
+	scale_x_datetime(name = '', expand = c(0,0)) + 
+	scale_y_continuous(name = 'fledging success', labels = scales::percent) + 
 	theme_fe +
-	theme(legend.position = 'none')
+	theme(legend.position = 'top', legend.direction = 'vertical', legend.box = "vertical", 
+				legend.margin = margin(), 
+				legend.box.margin = margin())
+
 pmsf
 
-grobs <- ggplotGrob(pm)$grobs
+grobs <- ggplotGrob(pmsf)$grobs
 legend <- grobs[[which(sapply(grobs, function(x) x$name) == "guide-box")]]
 
-s <- cowplot::plot_grid(pm + theme(legend.position = "none"), 
-												pmsf, ncol = 1, 
+s <- cowplot::plot_grid(pm, 
+												pmsf + theme(legend.position = "none"), ncol = 1, 
 												labels = c("A", "B"), label_size = 8, 
 												rel_heights = c(1, 1), 
 												align = "hv")
-s <- cowplot::plot_grid(legend, s, rel_heights = c(0.2, 1), ncol = 1)
-pdf("../figs/trend-all-locations.pdf", width = 3.5, height = 4.25)
+s <- cowplot::plot_grid(legend, s, rel_heights = c(0.4, 1), ncol = 1)
+pdf("../figs/trend-all-locations.pdf", width = 3.5, height = 5)
 s
 dev.off()
